b385cf78e559a8edd889ddf3d26b6ace7e1bf89f
Julia Lahdo <julia.lahdo@live.se>
2019-08-26 15:05:50 +0200

    Make tiny changes
    In hope for it to work better with Postman, Morgan

--- api/Post.php
+++ api/Post.php
@@ -7,11 +7,13 @@ class Post {
         $this->pdo = $pdo;
     }
 
-    public function post_booking($time_of_booking, $number_of_guests, $email, $name, $phone) {
+    public function post_booking($idBooking, $time_of_booking, $number_of_guests, $idCustomer, $email, $name, $phone) {
         $statement_booking = $this->pdo->prepare
             ("INSERT INTO booking (timeOfBooking, numberOfGuests) VALUES(:timeOfBooking, :numberOfGuests)");
             $statement_booking->execute(
                 [
+                    ":id" => $idBooking,
+                    ":customerId" => $customerId,
                     ":timeOfBooking" => $time_of_booking,
                     ":numberOfGuests" => $number_of_guests
                 ]
@@ -21,6 +23,7 @@ class Post {
             ("INSERT INTO customer (email, name, phone) VALUES(:email, :name, :phone)");
             $statement_customer->execute(
                 [
+                    ":id" => $idCustomer,
                     ":email" => $email,
                     ":name" => $name,
                     ":phone" => $phone
