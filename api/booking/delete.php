cc7206c9bec9d897356240c6523da59f260371df
eunjuhuss <eunjuhuss@hotmail.com>
2019-08-27 23:02:04 +0200

    change wrong database connection

--- api/booking/delete.php
+++ api/booking/delete.php
@@ -7,7 +7,7 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
 // Include database and object file
-include_once '../config/database.php';
+include_once '../config/Database.php';
 include_once '../Booking.php';
  
 // Get database connection
