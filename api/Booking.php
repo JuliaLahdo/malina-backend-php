ae358851edb4800ef7958ada6c7d617e903894a9
Morgan J <Moggelicious@hotmail.com>
2019-09-04 15:12:45 +0200

    this commit will delete properly

--- api/Booking.php
+++ api/Booking.php
@@ -1,7 +1,5 @@
 <?php
-
 class Booking {
-
     // Object properties
     public $dateOfBooking;
     public $timeOfBooking;
@@ -9,86 +7,70 @@ class Booking {
     public $email;
     public $name;
     public $phone;
+    
+    // Constructor with $database as database connection
     public function __construct($db) {
         $this->pdo = $db;
-}
-
+    }
+    // Read bookings
     function read() {
-
+        
+        // Select all query
         $readBookings = "SELECT * FROM customer AS customer
             INNER JOIN booking AS booking
             ON customer.id = booking.customerid
-            ORDER BY booking.id DESC";
-
+            ORDER BY booking.id DESC"; 
+        
+        // Prepare query statement
         $statement = $this->pdo->prepare($readBookings);
-
+        // Execute query
         $statement->execute();
-
-    return $statement;
-
+        return $statement;
     }
-
     // Create booking
     function create() {
-
         try{
-
             $fetchEmail = $this->pdo->prepare(
                 "SELECT * FROM customer WHERE email=:email"
             );
-
             $fetchEmail->bindParam("email", $this->email);
-
             $fetchEmail->execute();
-
             $rowCount = $fetchEmail->rowCount();
-
             if($rowCount > 0) {
                 $result = $fetchEmail->fetch(PDO::FETCH_ASSOC);
-
                 $bookingQuery = "INSERT INTO booking
                 SET customerId=:customerId,
                     dateOfBooking=:dateOfBooking,
                     timeOfBooking=:timeOfBooking,
                     numberofGuests=:numberOfGuests";
-
                 // Prepare booking query
                 $bookingStatement = $this->pdo->prepare($bookingQuery);
-                
                 $bookingStatement->execute([
                     ":customerId" => $result['id'],
                     ":dateOfBooking" => $this->dateOfBooking,
                     ":timeOfBooking" => $this->timeOfBooking,
                     ":numberOfGuests" => $this->numberOfGuests
                 ]);
-
             } else {
-
                 // Prepare customer query
                 $customerQuery = $this->pdo->prepare(
                     "INSERT INTO customer (email, name, phone) VALUES (:email, :name, :phone)"
                 );
-
                 $customerQuery->execute([
                     ":email" => $this->email,
                     ":name" => $this->name,
                     ":phone" => $this->phone,
                 ]);
-
                 // Select last inserted id (will be for customer added from query above)
                 $customerQuery = $this->pdo->prepare(
                     "SELECT LAST_INSERT_ID();"
                 );
-
                 $customerQuery->execute();
-
                 $lastInsertedId = $customerQuery->fetch(PDO::FETCH_NUM);
-
                 // Prepare booking query
                 $bookingQuery = $this->pdo->prepare(
                     "INSERT INTO booking (customerId, dateOfBooking, timeOfBooking, numberOfGuests) VALUES (:customerId, :dateOfBooking, :timeOfBooking, :numberOfGuests)"
                 );
-
                 $bookingQuery->execute([
                     ":customerId" => $lastInsertedId[0],
                     ":dateOfBooking" => $this->dateOfBooking,
@@ -97,35 +79,30 @@ class Booking {
                 ]);
         
             }
-
             return true;
             echo("Booking was created successfully");
-
         } catch (PDOException $error){
             return false;
             echo("Booking was not created successfully");
         }
     }
-
     // Update booking
     function update(){
-
         // Update booking
         $updateBooking = "UPDATE booking
             SET numberOfGuests=:numberOfGuests,
                 dateOfBooking=:dateOfBooking,
                 timeOfBooking=:timeOfBooking
                 WHERE id = :id";
-
+        
         $updateCustomer = "UPDATE customer
             SET numberOfGuests=:numberOfGuests,
                 dateOfBooking=:dateOfBooking,
                 timeOfBooking=:timeOfBooking
             WHERE id = :id";
-
         // Prepare query statement
         $statement = $this->pdo->prepare($query);
-
+    
         // Sanitize
         $this->id=htmlspecialchars(strip_tags($this->id));
         $this->dateOfBooking=htmlspecialchars(strip_tags($this->dateOfBooking));
@@ -134,7 +111,7 @@ class Booking {
         $this->email=htmlspecialchars(strip_tags($this->email));
         $this->name=htmlspecialchars(strip_tags($this->name));
         $this->phone=htmlspecialchars(strip_tags($this->phone));
-
+    
         // Bind new values
         $statement->bindParam(':id', $this->id);
         $statement->bindParam(':dateOfBooking', $this->dateOfBooking);
@@ -150,60 +127,45 @@ class Booking {
         }
         return false;
     }
-
     // Delete booking
     function deleteBooking(){
-
         // Delete booking query
         $deleteBooking = "DELETE FROM booking WHERE id = ?";
-
         // Prepare booking-query
         $deleteBookingStatement = $this->pdo->prepare($deleteBooking);
-
         // Bind id of record to delete
         $deleteBookingStatement->bindParam(1, $this->id);
-
         // Execute query
         if($deleteBookingStatement->execute()){
             return true;
         }
         return false;
     }
-
     // Delete customer data
     function deleteCustomer() {
-
         // Delete customer query
-        $deleteCustomer = "DELETE FROM customer WHERE id = :?";
-
+        $deleteCustomer = "DELETE FROM customer WHERE id = ?";
         // Prepare customer delete-query
         $deleteCustomerStatement = $this->pdo->prepare($deleteCustomer);
-
         // Bind id of record to delete
         $deleteCustomerStatement->bindParam(1, $this->id);
-
         // Execute query
         if($deleteCustomerStatement->execute()){
             return true;
         }
         return false;
     }
-
     function avaliableBookings() {
         // Check avaliable bookings query
         $avaliableBookings = "SELECT * FROM booking
         WHERE dateOfBooking=:dateOfBooking
         AND timeOfBooking=:timeOfBooking";
-
+        
         // Prepare query statement
         $statement = $this->pdo->prepare($avaliableBookings);
-
         // Execute query
         $statement->execute();
-
         return $statement;
     }
-
 }
-
 ?>
