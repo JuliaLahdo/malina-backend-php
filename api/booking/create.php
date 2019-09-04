e38d8ae984ad7bc05c8ca88168ee56fa425f9ba1
eunjuhuss <eunjuhuss@hotmail.com>
2019-09-04 12:51:39 +0200

    fix problems

--- api/booking/create.php
+++ api/booking/create.php
@@ -1,25 +1,25 @@
 <?php
-​
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-​
+
 // Get database connection
 include_once '../config/Database.php';
-​
+
 // Instantiate booking object
 include_once '../Booking.php';
-​
+
 $database = new Database();
 $db = $database->databaseConnection();
-​
+
 $booking = new Booking($db);
-​
+
 // Get posted data
 $data = json_decode(file_get_contents("php://input"));
-​
+
 // Make sure data is not empty
 if(
     !empty($data->dateOfBooking) &&
@@ -29,7 +29,6 @@ if(
     !empty($data->name) &&
     !empty($data->phone)
 ){
- 
     // Set booking property values
     $booking->dateOfBooking = $data->dateOfBooking;
     $booking->timeOfBooking = $data->timeOfBooking;
@@ -37,36 +36,36 @@ if(
     $booking->email = $data->email;
     $booking->name = $data->name;
     $booking->phone = $data->phone;
-​
+
     // Create the booking
     if($booking->create()){
- 
+
         // Set response code - 201 created
         http_response_code(201);
- 
+
         // Tell the user booking was created
         echo json_encode(array("message" => "Booking was created."));
     }
- 
+
     // If unable to create the booking, tell the user
     else{
- 
+
         // Set response code - 503 service unavailable
         http_response_code(503);
- 
+
         // Tell the user
         echo json_encode(array("message" => "Unable to create booking."));
     }
 }
-​
+
 // Tell the user data is incomplete
 else {
- 
+
     // Set response code - 400 bad request
     http_response_code(400);
- 
+
     // Tell the user
     echo json_encode(array("message" => "Unable to create booking. Data is incomplete."));
 }
-​
+
 ?>
--- api/booking/read.php
+++ api/booking/read.php
@@ -1,38 +1,22 @@
 <?php
-​
-// Required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-​
-// Get database connection
 include_once '../config/Database.php';
-​
-// Instantiate booking object
 include_once '../Booking.php';
-​
-// Instantiate database and product object
 $database = new Database();
 $db = $database->databaseConnection();
-​
-// Initialize object
 $booking = new Booking($db);
-​
-// Query products
+
 $statement = $booking->read();
 $number = $statement->rowCount();
-​
-// Check if more than 0 bookings are found
+
 if($number > 0) {
-​
-    //Bookingsarray
     $bookingsArray = array();
     $bookingsArray["bookings"] = array();
-​
-​
-    // Retrieve our table contents
+
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
         extract($row);
-​
+
         $singleBooking = array(
             "id" => $id,
             "customerId" => $customerId,
@@ -43,24 +27,16 @@ if($number > 0) {
             "name" => $name,
             "phone" => $phone
         );
- 
         array_push($bookingsArray["bookings"], $singleBooking);
     }
- 
-    // Set response code - 200 OK
+
     http_response_code(200);
- 
-    // Show products data in json format
     echo json_encode($bookingsArray);
 } else {
-    // Set response code - 404 Not found
     http_response_code(404);
-​
-    // Tell admin no bookings found
+
     echo json_encode(
         array("errorMessage" => "No bookings found.")
     );
 }
-​
-​
 ?>
